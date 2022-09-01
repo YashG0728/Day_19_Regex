ca4316aee6b8363c8ebe71345e7fe8b939318deb
YashG0728 <yashgharde07@gmail.com>
2022-09-01 23:32:41 +0530

    Added : At Least One Upper Case

--- PassWord.java
+++ PassWord.java
@@ -5,14 +5,14 @@ import java.util.regex.Pattern;
 public class PassWord {
     public static void main(String[] args) {
 
-        String[] passWord = {"Yash2828","Spasrh2028","bhavya2528"};
+        String[] passWord = {"YashGHARDe","SPArSHgharde","bHavyaGa04nDhi"};
 
         for (String passWords : passWord) {
             validatePassWord(passWords);
         }
     }
     private static void validatePassWord(String passWords) {
-        Pattern pattern = Pattern.compile("^[a-zA-z0-9]{8,}");
+        Pattern pattern = Pattern.compile("^(?=.*[A-Z])(?=.*[a-z][0-9])?.{8,}$");
 
         Matcher matcher = pattern.matcher(passWords);
 
@@ -20,7 +20,7 @@ public class PassWord {
             System.out.println(passWords + " " + ">>>" + " " + "[" + "Strong Password" + "]");
             return;
         }
-        System.out.println(passWords + " " + ">>>" + " " + "[" + "Try to make strong password" + "]");
+        System.out.println(passWords + " " + ">>>" + " " + "[" + "password Not Matched With Conditions" + "]");
     }
 }
 
